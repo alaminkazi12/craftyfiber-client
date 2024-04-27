a9914f1992a27fc7b1968b03cc267864d11fb96e
MD ALAMIN KAZI <kmalamin123210@gmail.com>
2024-04-27 20:30:16 +0600

    added add item

--- src/pages/AddCraft/AddCraft.jsx
+++ src/pages/AddCraft/AddCraft.jsx
@@ -1,7 +1,49 @@
 const AddCraft = () => {
   return (
-    <div>
-      <h1>Add Craft</h1>
+    <div className="mt-10">
+      <h2 className="  lg:text-3xl font-bold text-center text-[#B08D74]">
+        Add Craft
+      </h2>
+      <div className="w-1/2 mx-auto bg-[#F5F5F5] rounded-2xl p-6 mt-4">
+        <form>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Image</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Use Image Url"
+              name="image"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Item Name</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Item Name"
+              name="image"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Sub Catagory Name</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Use Image Url"
+              name="image"
+              className="input input-bordered"
+              required
+            />
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
