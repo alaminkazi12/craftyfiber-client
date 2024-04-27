cf1701dc9bab4184f3090f124ff587d6c4ecd68b
MD ALAMIN KAZI <kmalamin123210@gmail.com>
2024-04-27 20:47:34 +0600

    added add craft page input filed

--- src/pages/AddCraft/AddCraft.jsx
+++ src/pages/AddCraft/AddCraft.jsx
@@ -30,17 +30,37 @@ const AddCraft = () => {
               required
             />
           </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Sub Catagory Name</span>
-            </label>
-            <input
-              type="text"
-              placeholder="Use Image Url"
-              name="image"
-              className="input input-bordered"
-              required
-            />
+
+          <div className="flex  justify-between items-center">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Sub Catagory Name</span>
+              </label>
+              <select className="select select-bordered w-full max-w-xs">
+                <option disabled selected>
+                  Select the appropriate category name.
+                </option>
+                <option> Embroidery</option>
+                <option>Knitting & Crocheting</option>
+                <option>Knitting & Crocheting</option>
+                <option>Quilting</option>
+                <option>Beadwork</option>
+                <option>Tie-Dyeing</option>
+                <option>Macrame</option>
+              </select>
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Price</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Price"
+                name="price"
+                className="input input-bordered"
+                required
+              />
+            </div>
           </div>
         </form>
       </div>
