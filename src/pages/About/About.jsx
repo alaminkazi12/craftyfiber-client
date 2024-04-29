6e93de0c28f9c4f729cf5268358a3313511b9b62
MD ALAMIN KAZI <kmalamin123210@gmail.com>
2024-04-29 10:53:17 +0600

    added about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,7 +1,68 @@
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className="mt-14">
+      <h1 className="text-3xl font-bold text-[#B08D74] text-center">
+        About Us
+      </h1>
+      <div className="space-y-4">
+        <h2 className="text-2xl font-bold">Our Story</h2>
+        <p>
+          Welcome to CraftyFiber.com, your destination for beautifully crafted
+          textile arts and crafts!
+        </p>
+        <p>
+          At CraftyFiber, we believe in the power of creativity to inspire and
+          uplift. Our passion for handmade craftsmanship drives everything we
+          do, from sourcing the finest materials to delivering exceptional
+          customer service.
+        </p>
+
+        <h2 className="text-2xl font-bold">Our Mission</h2>
+        <p>
+          Our mission at CraftyFiber is simple: to celebrate the beauty of
+          handmade craftsmanship and to support artisans in sharing their
+          talents with the world. We strive to provide a curated selection of
+          high-quality craft items that inspire creativity and bring joy to our
+          customers' lives.
+        </p>
+
+        <h2 className="text-2xl font-bold">What We Offer</h2>
+        <p>
+          Explore our extensive collection of textile arts and crafts,
+          including:
+        </p>
+        <ul>
+          <li>1 . Embroidery</li>
+          <li>2. Knitting & Crocheting</li>
+          <li>3. Quilting</li>
+          <li>4. Beadwork</li>
+          <li>5. Tie-Dyeing</li>
+          <li>6. Macrame</li>
+        </ul>
+        <p>
+          Each item in our collection is handmade with care and attention to
+          detail, using eco-friendly materials whenever possible. By supporting
+          CraftyFiber, you're not just purchasing a product – you're investing
+          in artistry, tradition, and a more sustainable future.
+        </p>
+
+        <h2 className="text-2xl font-bold">Get in Touch</h2>
+        <p>
+          Thank you for choosing CraftyFiber.com for all your textile arts and
+          crafts needs. Whether you're a seasoned crafter or just starting your
+          creative journey, we're here to inspire and support you every step of
+          the way.
+        </p>
+        <p>
+          Have questions or feedback? We'd love to hear from you! Get in touch
+          with our friendly team at{" "}
+          <a href="mailto:info@craftyfiber.com">info@craftyfiber.com</a> or
+          connect with us on social media.
+        </p>
+
+        <p>Happy crafting!</p>
+        <p>The CraftyFiber Team</p>
+      </div>
     </div>
   );
 };
